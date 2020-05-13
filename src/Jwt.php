137929751b1934494d0e192f67a73ca383f80c68
安康 <oncwnuKxUu1G1rRrYG3Ph2LVqV8c@git.weixin.qq.com>
2020-05-13 16:10:08 +0800

    打撒奥所

---
+++ src/Jwt.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wangcb
+ * Date: 2020/5/13
+ * Time: 15:27
+ */
+
+namespace wangcb;
+
+
+class Jwt
+{
+    public function encode(){
+        return 'hello jwt!';
+    }
+
+}
